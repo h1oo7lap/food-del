cfbaab1600a0af71d03dd572472ca366e6fed1ba
Hun <h1oo7lap@gmail.com>
2025-10-29 10:27:45 +0700

    hoan thien fontend va backend

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,11 @@
+import order from "../models/orderModel.js";
+import userModel from "../models/userModel.js"
+import stripe from "stripe";
+
+
+// get placeing order fr
+const placeOrder = async (req, res) => {
+    
+}
+
+export { placeOrder }
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
-
+import userRouter from "./routes/userRouter.js";
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 
 // ap config
@@ -18,7 +21,10 @@ connectDB();
 
 // api endpoints
 app.use("/api/food", foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/images", express.static('uploads'))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 
 app.get("/", (req, res) => {
